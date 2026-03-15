e3e0222212069be51515e6cb009b5990bbf15d01
Ali Haggag <ali.haggag2005@gmail.com>
2026-03-15 19:24:43 +0200

    fix: update next.config.ts for Next.js latest compatibility

--- next.config.ts
+++ next.config.ts
@@ -5,38 +5,17 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "cdn.sanity.io" },
+      { protocol: "https", hostname: "placehold.co" },
     ],
   },
   experimental: {
-    ppr: "incremental",
-    after: true,
-  },
-  devIndicators: {
-    appIsrStatus: true,
-    buildActivity: true,
-    buildActivityPosition: "bottom-right",
+    cacheComponents: true,
   },
 };
 
@@ -45,12 +24,8 @@ export default withSentryConfig(nextConfig, {
   project: "cs-arena",
   silent: !process.env.CI,
   widenClientFileUpload: true,
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-  sourcemaps: {
-    disable: false,
-  },
+  reactComponentAnnotation: { enabled: true },
+  sourcemaps: { disable: false },
   disableLogger: true,
   automaticVercelMonitors: true,
 });
