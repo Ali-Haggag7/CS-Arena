fa78242e87af83d315c5fc2f5af77349c104e816
Ali Haggag <ali.haggag2005@gmail.com>
2026-03-22 15:54:13 +0200

    feat: complete sentry integration, feedback widget, and fix project view/edit logic

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,11 +6,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://a764d6aa1d66b9258b4cb094becdcb8e@o4507537249927168.ingest.de.sentry.io/4507537257922640",
+  dsn: "https://adaa4175f7743dff82fbc63627238807@o4511043734798336.ingest.us.sentry.io/4511043739910144",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
